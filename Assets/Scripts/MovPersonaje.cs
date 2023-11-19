012dab1fb0297c1a2cd028d9bc9739c8176e6fb9
Daniela Galleguillos Díaz <daniela.antonia.galleguillos@estudiantat.upc.edu>
2023-11-19 16:43:07 +0100

    ADD: Mov OK camara

--- Assets/Scripts/MovPersonaje.cs
+++ Assets/Scripts/MovPersonaje.cs
@@ -22,26 +22,46 @@ public class MovPersonaje : MonoBehaviour
 
 
     }
-    private void Update()
+    private void UpdateMovement()
     {
-
         float hor = Input.GetAxisRaw("Horizontal");
         float ver = Input.GetAxisRaw("Vertical");
 
         Vector3 velocity = Vector3.zero;
 
-        if(ver != 0  || hor != 0 )
+        if (ver != 0 || hor != 0)
         {
-            Vector3 direccion = (transform.forward * ver + transform.right * hor).normalized;
+            // Obtener la cámara del jugador (deberías cachear esto en Awake o Start si es posible)
+            Camera playerCamera = Camera.main;
+
+            // Usar la dirección en la que está mirando la cámara
+            Vector3 cameraForward = playerCamera.transform.forward;
+            Vector3 cameraRight = playerCamera.transform.right;
+
+            // Asegurarse de que el movimiento solo afecte al plano XZ, no a Y (arriba/abajo)
+            cameraForward.y = 0;
+            cameraRight.y = 0;
+
+            Vector3 direccion = (cameraForward * ver + cameraRight * hor).normalized;
 
             velocity = direccion * movementSpeed;
         }
 
-        velocity.y = rigidbody.velocity.y;
+        velocity.y = rigidbody.velocity.y; // Mantener la componente vertical de la velocidad
         rigidbody.velocity = velocity;
 
 
-        //transform.Translate(x * Time.deltaTime, 0, z * Time.deltaTime);
+    }
+
+    private void UpdateCameraLook()
+    {
+        float hor = Input.GetAxis("");
+    }
+
+    private void Update()
+    {
+        UpdateMovement();
+        UpdateCameraLook();
     }
     /*void Seleccionar()
     {
